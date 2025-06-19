01bf04570715327ad3b57bac585df551093f01d3
Niranjan A <niranjan.bengaluru@gmail.com>
2025-06-18 21:12:31 -0400

    New hierarchy and added info section for arrays

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const express = require("express");
-const path = require("path");
+const path = require("path");  
+
 const app = express();
 const port = 3000;
-
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'main.html'));
+app.use(express.static(path.join(__dirname, 'public'))); 
+app.get("/", (req, res) => {    
+  res.sendFile(path.join(__dirname, 'public', 'main.html'));
 });
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}!`);
+    console.log(`App listening on port ${port}!`);
 });
