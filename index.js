4654587901e62292fe26d02cccad4a1445a49163
Niranjan A <niranjan.bengaluru@gmail.com>
2025-06-21 16:21:31 -0400

    more of the same

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require("express");
-const path = require("path");  
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
-const port = 3000;
-app.use(express.static(path.join(__dirname, 'public'))); 
-app.get("/", (req, res) => {    
-  res.sendFile(path.join(__dirname, 'public', 'main.html'));
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'main.html'));
 });
+
 
 export default app;
